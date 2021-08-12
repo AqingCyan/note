64e35d60f6648c7c2ca6c99677c0637871257318
AqingCyan <AqingCyan@users.noreply.github.com>
2021-08-12 04:25:01 +0000

    deploy: 5e4c4d235e1487a37c756320e3017c3681ad6df9

--- links/index_props.js
+++ links/index_props.js
@@ -1,4 +1,4 @@
-import projectConfig from '/pagic.config.js';
+import projectConfig from '/note/pagic.config.js';
 export default {
     config: { "root": "/", ...projectConfig, branch: 'main' },
     'pagePath': "links/README.md",
@@ -12,7 +12,7 @@ export default {
     'script': React.createElement(React.Fragment, null,
         React.createElement("script", { src: "https://cdn.pagic.org/react@16.13.1/umd/react.production.min.js" }),
         React.createElement("script", { src: "https://cdn.pagic.org/react-dom@16.13.1/umd/react-dom.production.min.js" }),
-        React.createElement("script", { src: "/index.js", type: "module" })),
+        React.createElement("script", { src: "/note/index.js", type: "module" })),
     'contentTitle': React.createElement("h1", { key: "0" }, "\u53CB\u60C5\u94FE\u63A5"),
     'contentBody': React.createElement("article", { dangerouslySetInnerHTML: {
             __html: '<ul>\n<li><a href="https://blog.xcatliu.com/">xcatliu</a></li>\n</ul>'
